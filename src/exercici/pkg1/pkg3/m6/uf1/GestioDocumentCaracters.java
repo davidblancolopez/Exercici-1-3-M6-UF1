56107d17d5ddba5ed5e628540a78e113aa0be309
davidblancolopez <daviddbl94@gmail.com>
2017-02-07 20:39:43 +0100

    CAMBIOS

--- src/exercici/pkg1/pkg3/m6/uf1/GestioDocumentCaracters.java
+++ src/exercici/pkg1/pkg3/m6/uf1/GestioDocumentCaracters.java
@@ -4,27 +4,45 @@ import static java.io.FileDescriptor.in;
 import static java.io.FileDescriptor.out;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import static java.lang.System.in;
+import static java.lang.System.out;
+import static javax.management.Query.in;
+import static sun.misc.MessageUtils.out;
 
 public class GestioDocumentCaracters {
 
     String ruta1 = "C:\\proves\\Execici3-1";
     String ruta2 = "C:\\proves\\Execici3-2";
 
-    FileReader in = new Filereader(ruta1);
-    FileWriter out = new FileWriter(ruta2);
+    /**
+     * Metode per a la part 1.
+     * Crea els 2 fitxers que s'utilitzaran per llegir i escriure.
+     * Crida al metode copiaDades i li pasa els 2 fitxers.
+     */
+    public void copiarFitxersBuffers() {
+        try {
+            FileReader in = new FileReader(ruta1);
+            FileWriter out = new FileWriter(ruta2);
 
-    copiaDades(in, out);
+            copiaDades(in, out);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
     public void copiaDades(FileReader in, FileWriter out) {
         try {
-            Char[] dades = new char[100];
+            char[] dades = new char[100];
+
             int llegits = 0;
             while (-1 != (llegits = in.read(dades))) {
                 out.write(dades, 0, llegits);
             }
             out.close();
             in.close();
-        } catch (IOException ex) {...
+        } catch (IOException ex) {
+
         }
     }
 }
